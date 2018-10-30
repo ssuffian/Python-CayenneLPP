fbd3b21bd5645cdff08747b983e5d7a548679325
Stephen Suffian <stephen.suffian@gmail.com>
2018-10-30 00:47:09 -0400

    Updated license.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Open source library for python to decode CayenneLPP format payload.',
     url='https://github.com/OlegSomov/Python-CayenneLPP',
     author='Oleg Somov',
-    license='MIT',
+    license='Apache 2.0',
     packages=['python_cayennelpp'],
     zip_safe=False
 )
