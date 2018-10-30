461c7c0bdc4b1fc92555aa6b005285837bf834aa
Stephen Suffian <stephen.suffian@gmail.com>
2018-10-30 00:46:13 -0400

    Added setup.py so this can be installed

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Python-CayenneLPP',
+    version='0.1',
+    description='Open source library for python to decode CayenneLPP format payload.',
+    url='https://github.com/OlegSomov/Python-CayenneLPP',
+    author='Oleg Somov',
+    license='MIT',
+    packages=['python_cayennelpp'],
+    zip_safe=False
+)
+
